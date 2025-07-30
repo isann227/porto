4dbd64c3cb6b92b80ec63b0ab0f968402c44e794
isann227 <insanhakim227@gmail.com>
2025-07-30 16:00:57 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cupiditate tenetur fugiat! Consectetur maxime quia eos saepe quae minima id animi delectus expedita. Dignissimos excepturi modi hic dicta repudiandae nostrum quasi, nam nemo laboriosam! Voluptatem suscipit esse magnam distinctio dolores odit voluptate quidem hic minima error tempore ipsam nostrum qui, eius fuga doloribus sapiente iure maxime in. Aperiam aspernatur tempore quod reiciendis veritatis cum! Accusantium eligendi distinctio at nemo, voluptas nisi ut repudiandae, tempore inventore in itaque veniam sapiente unde quidem maxime ea similique, provident iusto amet tenetur quia. Illo quae inventore sunt molestias eaque modi facilis. Veritatis quia eius explicabo iste laudantium, esse id. Dolores accusantium incidunt debitis rem, quam dicta ipsa, iusto esse iste tenetur cumque hic beatae saepe quia impedit dolorum illo. Asperiores, voluptatem! Voluptatibus consequatur error repellat, enim nobis dolore. Harum eum cupiditate sit numquam ea corrupti ab porro nisi labore praesentium, dicta maiores dolores, asperiores autem? Beatae aliquid deserunt, quae provident a repellendus ea eos illum possimus libero corrupti nemo blanditiis consectetur aperiam distinctio veniam ipsa nihil vitae pariatur saepe dicta cupiditate doloribus voluptatibus? Id placeat amet quisquam odit eum eos repellat voluptas rerum, obcaecati laborum qui in. Ullam molestiae eveniet odit incidunt aspernatur! Sequi.</h1>
+    </>
+  )
+}
+
+export default App
