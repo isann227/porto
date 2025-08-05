6dbbdf25740c2550950422ea2d49716726a94b5a
isann227 <insanhakim227@gmail.com>
2025-08-05 20:21:58 +0700

    update [proyek section]

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import DataImage from "./data"
+import { listTools, listProyek } from "./data"
+
 
 function App() {
 
@@ -7,7 +9,7 @@ function App() {
       <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
         <div>
           <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
-            <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" />
+            <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" loading="lazy" />
             <q>kode yang indah, lahir dari ketekunan.😍</q>
           </div>
           <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Insan Hakim</h1>
@@ -19,18 +21,18 @@ function App() {
             <a href="#" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600">Lihat Proyek <i className="ri-arrow-down-line ri-lg"></i></a>
           </div>
         </div>
-        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" />
+        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" loading="lazy" />
       </div>
 
       {/* Tentang */}
       <div className="tentang mt-32 py-10">
         <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
-          <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" />
+          <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" loading="lazy" />
           <p className="text-base/loose mb-10">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptatem tenetur dolorum rerum temporibus eveniet ipsa at praesentium illum laudantium, repudiandae quis est, accusamus obcaecati porro mollitia nisi, accusantium dolor! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus maiores, dolores fugit esse provident nostrum, vero voluptates ut exercitationem ab excepturi culpa impedit fugiat itaque. Dolor eius quasi suscipit commodi!
           </p>
           <div className="flex items-center justify-between">
-            <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md sm:block hidden" />
+            <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md sm:block hidden" loading="lazy" />
             <div className="flex items-center gap-6">
               <div>
                 <h1 className="text-4xl mb-1">
@@ -50,21 +52,49 @@ function App() {
 
         <div className="tools mt-32">
           <h1 className="text-4xl/snug font-bold mb-4">Tools yang dipakai</h1>
-          <p className="w-2/5 text-base/loose opacity-50">Berikut ini beberapa tools yang biasa saya pakai untuk pembuatan website atauoun desain</p>
-          <div className="tools-box">
-            <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md
-            hover:bg-zinc-800 group">
-              <img src="" alt="Tools Image" className="w-14 bg-zinc-800 p-1
-               group-hover:bg-zinc-900" />
-              <div>
-                <h4>Nama Tools</h4>
-                <p>Desain App</p>
+          <p className="xl:w-2/5lg lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">Berikut ini beberapa tools yang biasa saya pakai untuk pembuatan website atauoun desain</p>
+          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+            {listTools.map(tool => (
+              <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md
+            hover:bg-zinc-800 group" key={tool.id}>
+                <img src={tool.gambar} alt="Tools Image" className="w-14 bg-zinc-800 p-1
+               group-hover:bg-zinc-900" loading="lazy" />
+                <div>
+                  <h4 className="font-bold ">{tool.nama}</h4>
+                  <p className="opacity-50">{tool.ket}</p>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
       {/* Tentang */}
+
+      {/* Proyek */}
+      <div className="proyek mt-32 py-10">
+        <h1 className="text-center text-4xl font-bold mb-2">Proyek</h1>
+        <p className="text-base/loose text-center opacity-50">Berikut ini beberapa proyek yang telah saya buat.</p>
+        <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+          {listProyek.map(proyek => (
+            <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md">
+              <img src={proyek.gambar} alt="Proyek Image" loading="lazy" />
+              <div className="flex-wrap gap-2">
+                <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
+                <p className="text-base/loose mb-4">{proyek.desk}</p>
+                <div>
+                  {proyek.tools.map((tool, index) => (
+                    <p className="py-1 px-3 border border-zinc-500 bg-zinc-600 rounded-md font-semibold" key={index}>{tool}</p>
+                  ))}
+                </div>
+                <div className="mt-8 text-center">
+                  <a href="#" className="bg-violet-700 p-3 rounded-lg block border border-zinc-600 hover:bg-violet-600">Lihat Website</a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Proyek */}
     </>
   )
 }
