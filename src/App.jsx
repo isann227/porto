258be7aef355cb5690310a6a6d70d2842e672f79
isann227 <insanhakim227@gmail.com>
2025-08-06 23:30:58 +0700

    update [Footer section, PreLoader, Animated]

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
-        <div>
+        <div className="animate__animated animate__fadeInUp animate__delay-3s">
           <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
             <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" loading="lazy" />
             <q>kode yang indah, lahir dari ketekunan.😍</q>
@@ -18,15 +18,15 @@ function App() {
           </p>
           <div className="flex items-center sm:gap-4 gap-2">
             <a href="#" className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600">Download CV <i className="ri-download-line ri-lg"></i></a>
-            <a href="#" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600">Lihat Proyek <i className="ri-arrow-down-line ri-lg"></i></a>
+            <a href="#proyek" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600">Lihat Proyek <i className="ri-arrow-down-line ri-lg"></i></a>
           </div>
         </div>
-        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" loading="lazy" />
+        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto animate__animated animate__fadeInUp animate__delay-4s" loading="lazy" />
       </div>
 
       {/* Tentang */}
-      <div className="tentang mt-32 py-10">
-        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+      <div className="tentang mt-32 py-10" id="tentang">
+        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
           <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" loading="lazy" />
           <p className="text-base/loose mb-10">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptatem tenetur dolorum rerum temporibus eveniet ipsa at praesentium illum laudantium, repudiandae quis est, accusamus obcaecati porro mollitia nisi, accusantium dolor! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus maiores, dolores fugit esse provident nostrum, vero voluptates ut exercitationem ab excepturi culpa impedit fugiat itaque. Dolor eius quasi suscipit commodi!
@@ -51,12 +51,13 @@ function App() {
         </div>
 
         <div className="tools mt-32">
-          <h1 className="text-4xl/snug font-bold mb-4">Tools yang dipakai</h1>
-          <p className="xl:w-2/5lg lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">Berikut ini beberapa tools yang biasa saya pakai untuk pembuatan website atauoun desain</p>
+          <h1 className="text-4xl/snug font-bold mb-4" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Tools yang dipakai</h1>
+          <p className="xl:w-2/5lg lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50"
+            data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">Berikut ini beberapa tools yang biasa saya pakai untuk pembuatan website atauoun desain</p>
           <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
             {listTools.map(tool => (
               <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md
-            hover:bg-zinc-800 group" key={tool.id}>
+            hover:bg-zinc-800 group" key={tool.id} data-aos="fade-up" data-aos-duration="1000" data-aos-delay={tool.dad} data-aos-once="true">
                 <img src={tool.gambar} alt="Tools Image" className="w-14 bg-zinc-800 p-1
                group-hover:bg-zinc-900" loading="lazy" />
                 <div>
@@ -71,12 +72,12 @@ function App() {
       {/* Tentang */}
 
       {/* Proyek */}
-      <div className="proyek mt-32 py-10">
-        <h1 className="text-center text-4xl font-bold mb-2">Proyek</h1>
-        <p className="text-base/loose text-center opacity-50">Berikut ini beberapa proyek yang telah saya buat.</p>
+      <div className="proyek mt-32 py-10" id="proyek">
+        <h1 className="text-center text-4xl font-bold mb-2" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Proyek</h1>
+        <p className="text-base/loose text-center opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">Berikut ini beberapa proyek yang telah saya buat.</p>
         <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
           {listProyek.map(proyek => (
-            <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md">
+            <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md" data-aos="fade-up" data-aos-duration="1000" data-aos-delay={proyek.dad} data-aos-once="true">
               <img src={proyek.gambar} alt="Proyek Image" loading="lazy" />
               <div className="flex-wrap gap-2">
                 <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
@@ -97,11 +98,13 @@ function App() {
       {/* Proyek */}
 
       {/* Kontak */}
-      <div className="kontak mt-32 p-10">
-        <h1 className="text-4xl mb-2 font-bold text-center">Kontak</h1>
-        <p className="text-base/loose text-center mb-10 opacity-50">Mari terhubung dengan saya.</p>
+      <div className="kontak mt-32 sm:p-10 p-0" id="kontak">
+        <h1 className="text-4xl mb-2 font-bold text-center" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Kontak</h1>
+        <p className="text-base/loose text-center mb-10 opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">Mari terhubung dengan saya.</p>
       </div>
-      <form action="https://formsubmit.co/insanhakim227@gmail.com" method="POST" className="bg-zinc-800 p-10 w-fit mx-auto rounded-md" autoComplete="off">
+      <form action="https://formsubmit.co/insanhakim227@gmail.com" method="POST"
+        className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md" autoComplete="off"
+        data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500" data-aos-once="true">
         <div className="flex flex-col gap-6">
           <div className="flex flex-col gap-2">
             <label className="font-semibold">Nama lengkap</label>
