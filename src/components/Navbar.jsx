e74a427860a749c9b3220dc004fab460aa7a4c19
isann227 <insanhakim227@gmail.com>
2025-08-07 23:40:52 +0700

    update [ typo ]

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <div className="navbar py-7 flex items-center justify-between">
       <div className="logo">
         <h1 className="text-3xl font-bold bg-white text-black p-1 md:bg-transparent
-        md:text-white">Portfolio</h1>
+        md:text-white">Portofolio</h1>
       </div>
       <ul className={`menu flex items-center sm:gap-10 gap-4 md:static fixed left-1/2 -translate-x-1/2 
       md:-translate-x-0 md:opacity-100 bg-white/30 backdrop-blur-md p-4 rounded-2xl rounded-bl-2xl md:bg-transparent transition-all md:transition-none z-40 ${active ? "top-0 opacity-100" : "-top-10 opacity-0"}`}>
